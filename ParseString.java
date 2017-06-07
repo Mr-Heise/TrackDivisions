db3a377a1dad833868c9438a154458b49bc7b08e
Mr-Heise <mr.heise@gmail.com>
2017-06-07 10:58:59 -0600

    Add files via upload

---
+++ ParseString.java
@@ -0,0 +1,31 @@
+/*
+ * ParseString.java
+ * Mr. Heise
+ * CSC30
+ * Start Date: Wednesday 7 June 2017
+ * This program simulates processing a line of text from a text file.
+ */
+
+
+/**
+ * The ParseString class simulates processing a line of text from a text file.
+ */
+
+
+public class ParseString {
+
+	public static void main(String[] args){
+	
+		String lineOfText = "Babyak,Akilah,2000,6,June";
+		
+		
+		System.out.println("\n\t*** THE PARSE STRING SIMULATION PROGRAM ***\n");
+		
+		
+	
+	
+	
+	}
+
+
+}
