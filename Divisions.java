bb6e01c58d159189e7d4f008049e1a206814b0d5
Mr-Heise <mr.heise@gmail.com>
2017-06-08 10:58:13 -0600

    Add files via upload

---
+++ Divisions.java
@@ -0,0 +1,31 @@
+/*
+ * Divisions.java
+ * Mr. Heise
+ * CSC30
+ * Start Date: Thursday 8 June 2017
+ * Create a program that sorts students into track divisions using a comma-delimited file. All results will
+ *	be exported into a divisions.txt file.
+ */
+
+import java.io.*;
+
+/**
+ * The Divisions class sorts students into track divisions using a comma-delimited file. All results will be 
+ *	exported into a divisions.txt file.
+ */
+
+public class Divisions{
+
+	public static void main(String[] args){
+	
+	
+		System.out.println("\n\t*** THE TRACK DIVISIONS PROGRAM ***\n");
+		
+		
+	
+	
+	}
+
+
+
+}
