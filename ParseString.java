d99b30c84d4af89d5bfe969f0f2778d4daa471cd
Mr-Heise <mr.heise@gmail.com>
2017-06-08 10:39:35 -0600

    Add files via upload

--- ParseString.java
+++ ParseString.java
@@ -7,6 +7,8 @@
  */
 
 
+
+
 /**
  * The ParseString class simulates processing a line of text from a text file.
  */
@@ -18,9 +20,72 @@ public class ParseString {
 	
 		String lineOfText = "Babyak,Akilah,2000,6,June";
 		
+		String lastName;
+		String firstName;
+		
+		
+		int birthYear;
+		int birthMonth;
+		
+		int comma;
+		
+		
+		
 		
 		System.out.println("\n\t*** THE PARSE STRING SIMULATION PROGRAM ***\n");
 		
+		
+		comma = lineOfText.indexOf(",");
+		
+		lastName = lineOfText.substring(0,comma);
+		
+		System.out.println("\n\tlastName = " + lastName);
+		
+		
+		
+		lineOfText = lineOfText.substring(comma + 1);
+		
+		//System.out.println("\n\tlineOfText = " + lineOfText);
+		
+		
+		
+		comma = lineOfText.indexOf(",");
+		
+		firstName = lineOfText.substring(0,comma);
+		
+		System.out.println("\n\tfirstName = " + firstName);
+		
+		
+		
+		lineOfText = lineOfText.substring(comma + 1);
+		
+		//System.out.println("\n\tlineOfText = " + lineOfText);
+		
+		
+		
+		comma = lineOfText.indexOf(",");
+		
+		birthYear = Integer.parseInt(lineOfText.substring(0,comma));
+		
+		System.out.println("\n\tbirthYear = " + birthYear);
+		
+		
+	
+		lineOfText = lineOfText.substring(comma + 1);
+	
+		//System.out.println("\n\tlineOfText = " + lineOfText);
+		
+		
+		comma = lineOfText.indexOf(",");
+		
+		birthMonth = Integer.parseInt(lineOfText.substring(0,comma));
+		
+		System.out.println("\n\tbirthMonth = " + birthMonth);
+		
+		
+		
+		
+		System.out.println();
 		
 	
 	
